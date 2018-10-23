6b1a0a3d7dfb21fc97de346e1c48dad1cdc7f4c0
shaqianqian <sqq940814@gmail.com>
2018-10-23 15:33:38 +0200

    init

---
+++ affichageSoutenances.js
@@ -0,0 +1,15 @@
+//
+// var Plan = (function () {
+//     function Plan(Specialite, Salle,CouleursPanel,Soutenance) {
+//         this.Specialite = Specialite;
+//         this.Salle = Salle;
+//         this.CouleursPanel = CouleursPanel;
+//         this.Soutenance = Soutenance;
+//     }
+//     // Plan.prototype.SayHello = function () {
+//     //     window.alert("My specialite is " + this.Specialite + ".");
+//     // };
+//     return Plan;
+// })();
+//
+
